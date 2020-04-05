1cc68fd3c668fc20dde6d4b407323c5ea59158a5
daeheum <rlaeogma12@naver.com>
2020-04-05 23:28:18 +0900

    first commit

---
+++ src/main/java/io/honeymon/tacademy/springboot/domain/Book.java
@@ -0,0 +1,22 @@
+package io.honeymon.tacademy.springboot.domain;
+
+import javax.persistence.Entity;
+
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Book extends AbstractPersistable<Long> {
+
+	private String name;
+	private String isbn13;
+	private String isbn10;
+	
+	
+}
---
+++ src/main/java/io/honeymon/tacademy/springboot/service/BookService.java
@@ -0,0 +1,9 @@
+package io.honeymon.tacademy.springboot.service;
+
+import java.util.Optional;
+import io.honeymon.tacademy.springboot.domain.Book;
+
+public interface BookService {
+
+	Optional<Book> findById(Long id);
+}
